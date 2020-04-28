eb04eea084e00a1dce7b5ccc421b864c2130e176
Orestis Tsioukis <orestistsioukis@gmail.com>
2020-04-28 18:24:35 +0300

    Add a new console message

--- TinyCrm/Program.cs
+++ TinyCrm/Program.cs
@@ -6,7 +6,9 @@ namespace TinyCrm
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World 2!");
         }
     }
+    
 }
