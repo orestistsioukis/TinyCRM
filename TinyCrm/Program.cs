0d3a81c56ba71b224cd5397550942d16b282eed2
Orestis Tsioukis <orestistsioukis@gmail.com>
2020-04-28 21:48:39 +0300

    creating a new customer

--- TinyCrm/Program.cs
+++ TinyCrm/Program.cs
@@ -6,8 +6,14 @@ namespace TinyCrm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World 2!");
+            try
+            {
+                var dpnevmatikos = new Customer("123456789");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
         }
     }
     
